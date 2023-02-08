7dccbfabf24d3a7d8853edef0007c35316de2f98
Alex-Ennerfelt <alex.ennerfelt@gmail.com>
2023-02-08 18:01:57 +0800

    Added buttons for loglevel to the settings menu

--- Editor/PowerDebugLogThresholdEditor.cs
+++ Editor/PowerDebugLogThresholdEditor.cs
@@ -21,11 +21,32 @@ static class PowerDebugLogThresholdEditor
                 rootElement.styleSheets.Add(styleSheet);
 
                 var levelSlider = rootElement.Q<SliderInt>("loglevel-slider");
+                var buttonLow = rootElement.Q<Button>("button_low");
+                var buttonMedium = rootElement.Q<Button>("button_medium");
+                var buttonHigh = rootElement.Q<Button>("button_high");
+
                 levelSlider.value = settings.Number;
 
                 levelSlider.RegisterValueChangedCallback(level => {
                     settings.Number = level.newValue;
                 });
+
+                buttonLow.clicked += () => {
+                    settings.Number = (int)LogThreshold.Low;
+                    OnNumberChanged(settings.Number);
+                };
+                buttonMedium.clicked += () => {
+                    settings.Number = (int)LogThreshold.Medium;
+                    OnNumberChanged(settings.Number);
+                };
+                buttonHigh.clicked += () => {
+                    settings.Number = (int)LogThreshold.High;
+                    OnNumberChanged(settings.Number);
+                };
+
+                void OnNumberChanged(int number) {
+                    levelSlider.value = number;
+                }
             },
 
             // Populate the search keywords to enable smart search filtering and label highlighting:
